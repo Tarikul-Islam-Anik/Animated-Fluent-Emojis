5f4d5d4d04c81f9feea234373fef338f661f597b
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2022-08-18 20:15:08 +0600

    Updated title.

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="system" enableSystem={true} attribute="class">
       <Head>
-        <title>Microsoft Teams - Animated Emojis</title>
+        <title>Animated Fluent Emojis - Microsoft</title>
         <link rel="shortcut icon" href="/favicon.png" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
