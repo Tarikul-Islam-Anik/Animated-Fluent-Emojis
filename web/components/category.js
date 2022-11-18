e2f3c9307bc8177d73953a024e872c6fea592c0b
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2022-11-18 14:38:38 +0600

    Code clean up.

--- web/components/category.js
+++ web/components/category.js
@@ -1,5 +1,17 @@
 import Link from 'next/link';
 
+const categories = [
+  'Smiles',
+  'Hand gestures',
+  'People',
+  'Animals',
+  'Food',
+  'Travel and places',
+  'Objects',
+  'Activities',
+  'Symbols'
+];
+
 export const Category = () => {
   return (
     <section
@@ -7,51 +19,13 @@ export const Category = () => {
     max-w-screen-lg flex-wrap justify-evenly gap-5 rounded-lg bg-slate-100/50 p-4 backdrop-blur-sm 
     dark:bg-slate-800/75"
     >
-      <Link href="#smilies">
-        <a className="rounded-md bg-slate-200/60 py-1.5 px-2 text-slate-700 transition hover:scale-[1.05] dark:bg-slate-700/80 dark:text-slate-300">
-          Smiles
-        </a>
-      </Link>
-      <Link href="#hand-gestures">
-        <a className="rounded-md bg-slate-200/60 py-1.5 px-2 text-slate-700 transition hover:scale-[1.05] dark:bg-slate-700/80 dark:text-slate-300">
-          Hand gestures
-        </a>
-      </Link>
-      <Link href="#people">
-        <a className="rounded-md bg-slate-200/60 py-1.5 px-2 text-slate-700 transition hover:scale-[1.05] dark:bg-slate-700/80 dark:text-slate-300">
-          People
-        </a>
-      </Link>
-      <Link href="#animals">
-        <a className="rounded-md bg-slate-200/60 py-1.5 px-2 text-slate-700 transition hover:scale-[1.05] dark:bg-slate-700/80 dark:text-slate-300">
-          Animals
-        </a>
-      </Link>
-      <Link href="#food">
-        <a className="rounded-md bg-slate-200/60 py-1.5 px-2 text-slate-700 transition hover:scale-[1.05] dark:bg-slate-700/80 dark:text-slate-300">
-          Food
-        </a>
-      </Link>
-      <Link href="#travel-and-places">
-        <a className="rounded-md bg-slate-200/60 py-1.5 px-2 text-slate-700 transition hover:scale-[1.05] dark:bg-slate-700/80 dark:text-slate-300">
-          Travel and places
-        </a>
-      </Link>
-      <Link href="#objects">
-        <a className="rounded-md bg-slate-200/60 py-1.5 px-2 text-slate-700 transition hover:scale-[1.05] dark:bg-slate-700/80 dark:text-slate-300">
-          Objects
-        </a>
-      </Link>
-      <Link href="#activities">
-        <a className="rounded-md bg-slate-200/60 py-1.5 px-2 text-slate-700 transition hover:scale-[1.05] dark:bg-slate-700/80 dark:text-slate-300">
-          Activities
-        </a>
-      </Link>
-      <Link href="#symbols">
-        <a className="rounded-md bg-slate-200/60 py-1.5 px-2 text-slate-700 transition hover:scale-[1.05] dark:bg-slate-700/80 dark:text-slate-300">
-          Symbols
-        </a>
-      </Link>
+      {categories.map((category) => (
+        <Link href={`#${category.toLowerCase().replace(' ', '-').replace(' ', '-')}`}>
+          <a className="rounded-md bg-slate-200/60 py-1.5 px-2 text-slate-700 transition hover:scale-[1.05] dark:bg-slate-700/80 dark:text-slate-300">
+            {category}
+          </a>
+        </Link>
+      ))}
     </section>
   );
 };
